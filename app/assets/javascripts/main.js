c5adc5066b7d9b175d326a32e818578fad851eab
jamalp <jamalkpowell@gmail.com>
2014-02-03 19:44:46 -0500

    fancybox implentation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,13 +1,7 @@
 $(function (){
-  $('.page-header').click(function(){
-    $('.thumbnails').addClass('owl-carousel');
-    $("#owl").owlCarousel({
-         autoPlay: 3000, //Set AutoPlay to 3 seconds
-      items : 2,
-
-   
-    })();
-  })
- 
-
+  $(".thumbnail").click(function() {
+ 		$.fancybox.open([{href : $(this).find('img').attr('src')}]);
+        return false;
+    });
 });
+
